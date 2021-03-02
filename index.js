c7c7f4120628331324d8639239caced0882d63a9
Massimilianok <massimiliano.rizzuto87@gmail.com>
2021-03-02 17:38:25 +0100

    feat: Connect Blog articles database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const articles = require('./router/articles');
--- router/articles.js
+++ router/articles.js
@@ -1,21 +1,39 @@
 const express = require('express');
-const articles = require('../blog');
+const { Pool } = require('pg');
 
 const router = express.Router();
+const pool = new Pool();
 
 router.get('/', (req, res) => {
-  res.status(200).json({ code: 200, message: 'OK', data: articles });
+  pool
+    .query('SELECT * FROM articles ORDER BY articleid')
+    .then((data) =>
+      res.status(200).json({ code: 200, message: 'OK', data: data.rows })
+    )
+    .catch((err) =>
+      res.status(500).json({ code: 500, error: 'Internal server errors!' })
+    );
 });
 
 router.get('/:id', (req, res) => {
-  const article = articles.find(
-    (article) => article.id === parseInt(req.params.id)
-  );
-  if (article) {
-    res.status(200).json({ code: 200, message: 'OK', data: article });
-  } else {
-    res.status(404).json({ code: 404, error: 'Article does not exist!' });
-  }
+  const { id } = req.params;
+  const getOneArticle = {
+    text: 'SELECT * FROM articles WHERE articleid=$1',
+    values: [id],
+  };
+
+  pool
+    .query(getOneArticle)
+    .then((data) => {
+      if (data.rows.length !== 0) {
+        res.status(200).json({ code: 200, message: 'OK', data: data.rows });
+      } else {
+        res.status(404).json({ code: 404, error: 'Article does not exist!' });
+      }
+    })
+    .catch((err) =>
+      res.status(500).json({ code: 500, error: 'Internal server errors!' })
+    );
 });
 
 module.exports = router;
