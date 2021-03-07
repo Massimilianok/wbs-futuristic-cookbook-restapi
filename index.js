9e41b4d81324e1d8c702f8f1a040dcbf2ee5f016
Massimilianok <massimiliano.rizzuto87@gmail.com>
2021-03-07 16:38:22 +0100

    feat: Add new route to create article

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const articles = require('./router/articles');
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use('/api/articles', articles);
--- router/articles.js
+++ router/articles.js
@@ -6,7 +6,7 @@ const pool = new Pool();
 
 router.get('/', (req, res) => {
   pool
-    .query('SELECT * FROM articles ORDER BY articleid')
+    .query('SELECT * FROM blogarticles ORDER BY articleid')
     .then((data) =>
       res.status(200).json({ code: 200, message: 'OK', data: data.rows })
     )
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   const getOneArticle = {
-    text: 'SELECT * FROM articles WHERE articleid=$1',
+    text: 'SELECT * FROM blogarticles WHERE articleid=$1',
     values: [id],
   };
 
@@ -31,6 +31,26 @@ router.get('/:id', (req, res) => {
         res.status(404).json({ code: 404, error: 'Article does not exist!' });
       }
     })
+    .catch((err) =>
+      res.status(500).json({ code: 500, error: 'Internal server errors!' })
+    );
+});
+
+router.post('/create', (req, res) => {
+  const { articleID, title, description, category } = req.body;
+  const createOneArticle = {
+    text:
+      'INSERT INTO blogarticles (articleID, title, description, category) VALUES ($1, $2, $3, $4) RETURNING *',
+    values: [articleID, title, description, category],
+  };
+
+  pool
+    .query(createOneArticle)
+    .then((data) =>
+      res
+        .status(201)
+        .json({ code: 201, message: 'Article created', data: data.rows })
+    )
     .catch((err) =>
       res.status(500).json({ code: 500, error: 'Internal server errors!' })
     );
